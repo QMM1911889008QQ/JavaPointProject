7e954957f2b5afa604dc9e49afbba61dfc86f3bf
qi.miaomiao <1911889008@qq.com>
2020-03-20 21:56:41 +0800

    插入排序

--- src/JavaMain/Java/com/qmm/javaword/process/ArrayInsertSort.java
+++ src/JavaMain/Java/com/qmm/javaword/process/ArrayInsertSort.java
@@ -25,7 +25,7 @@ public class ArrayInsertSort {
     public static void insertSort(int[] a){
         if(a != null){
             for(int i = 1;i<a.length;i++){
-                int temp = a[i],j=i;
+                int temp = a[i],j = i;
                 if(a[j-1] > temp){
                     while(j >= 1 && a[j-1] > temp){
                         a[j] = a[j-1];
